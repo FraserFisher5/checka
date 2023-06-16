717340a8ead1f9c27dbf986fad67fb9e9bfce31f
superviss <82835778+superviss@users.noreply.github.com>
2023-06-16 15:49:25 +0700

    Create gone.py

---
+++ gone.py
@@ -0,0 +1,20 @@
+import numpy
+>>> x = 6
+
+>>> while x < 15:
+	print(x)
+	x += 1
+  >>> x = 4
+
+>>> while x >= 0:
+	print("Hello" * x)
+	x -= 1
+  >>> x = 5
+
+>>> while x < 15:
+	if x % 2 == 0:
+		print("Even:", x)
+		break
+	print(x)
+	x += 1
+  
